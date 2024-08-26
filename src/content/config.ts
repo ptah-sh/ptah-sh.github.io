12cd6fdc45d6fba7d461bcb4c84c61879595f8cf
Bohdan Shulha <b.shulha@pm.me>
2024-08-26 11:42:37 +0200

    feat: add tags to templates

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,4 +5,5 @@ export const collections = {
   docs: defineCollection({ schema: docsSchema() }),
   blog: defineCollection({ schema: docsSchema() }),
   marketplace: defineCollection({ type: "data" }),
+  marketplaceTags: defineCollection({ type: "data" }),
 };
