b10f25854dd71ead562d2af9f27ed0960d355238
Bohdan Shulha <bv.shulha@gmail.com>
2024-09-28 14:25:51 +0200

    feat: better linking

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,9 +1,18 @@
-import { defineCollection } from "astro:content";
+import { defineCollection, z } from "astro:content";
 import { docsSchema } from "@astrojs/starlight/schema";
 
+const customDocsSchema = () => {
+  return docsSchema({
+    extend: z.object({
+      relatedApps: z.array(z.string()).optional(),
+      relatedTags: z.array(z.string()).optional(),
+    }),
+  });
+};
+
 export const collections = {
-  docs: defineCollection({ schema: docsSchema() }),
-  blog: defineCollection({ schema: docsSchema() }),
+  docs: defineCollection({ schema: customDocsSchema() }),
+  blog: defineCollection({ schema: customDocsSchema() }),
   marketplace: defineCollection({ type: "data" }),
   marketplaceTags: defineCollection({ type: "data" }),
   alternatives: defineCollection({ type: "data" }),
